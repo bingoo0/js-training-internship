6b18a55958f5b335b9bda6d2cd9c502bd0e561fd
bingoo0 <mehmedsezgin01@gmail.com>
2018-02-14 13:51:38 +0200

    add task: pw1-002

---
+++ listEvent.js
@@ -0,0 +1,19 @@
+function listEvent(collection) {
+
+    return Object.keys(collection).forEach(function(key) {
+        console.log(collection[key]);
+    });
+}
+
+createEvent({
+    1: {
+        eventName: 'Sample Event',
+        event: ' Grand Opening of new Club',
+        access: true
+    },
+    2: {
+        clientName: 'Ivan Ivanov',
+        gender: 'male',
+        age: 18
+    }
+});
