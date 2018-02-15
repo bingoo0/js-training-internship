1039e0e694eaff859cd5173eedd90d4166eef33b
bingoo0 <mehmedsezgin01@gmail.com>
2018-02-15 16:03:45 +0200

    fixed tasks pw1-m2- 001 002 003 004 005

--- collections.js
+++ collections.js
@@ -1,57 +1,121 @@
-var forEachFunc = function(collection) {
+var forEachFunc = function(collection, callback) {
 
-    return collection.forEach(element => {
+    var result = [];
+    for (let i = 0; i < collection.length; i++) {
 
-        console.log(element);
+        result[i] = callback(collection[i]);
 
-    });
+    }
+
+    return result;
+}
+
+function callbackFunc(collection) {
+
+    return collection;
 }
 
 console.log("--- For Each Function ---")
 
-var printForEachFunc = forEachFunc([1, 2, 3, 4]);
+
+var printForEachFunc = forEachFunc([1, 2, 3, 4], callbackFunc);
+console.log(printForEachFunc);
+
+
 
 console.log("--- Filter Function ---")
 
-var filterFunc = function(collection) {
+var filterFunc = function(collection, callback) {
 
-    return collection.filter(element => {
+    var result = [];
+    for (let i = 0; i < collection.length; i++) {
 
-        if (element % 2 == 0) {
-            console.log(element);
+        if (collection[i] % 2 == 0) {
+            result[i] = callback(collection[i]);
         }
-    })
+    }
+
+    return result;
+
 }
 
-var printFilterFunc = filterFunc([2, 4, 5, 19, 12]);
+function filter(collection) {
+
+    return collection;
+}
+
+var printFilterFunc = filterFunc([2, 4, 5, 19, 12], filter);
+console.log(printFilterFunc);
+
 
 console.log("--- Map Function ---")
 
-var mapFunc = function(collection) {
+var mapFunc = function(collection, callback) {
 
-    return collection.map(element => {
-        console.log(element * 3);
-    })
+    var result = [];
+    for (let i = 0; i < collection.length; i++) {
+
+        result[i] = callback(collection[i]);
+
+    }
+
+    return result;
 }
 
-var printMapFunc = mapFunc([1, 2, 3, 4]);
+function map(collection) {
+
+    return collection * 2;
+}
+
+var printMapFunc = mapFunc([1, 2, 3, 4], map);
+
+console.log(printMapFunc);
+
 
 console.log("--- Fill Function ---")
 
 var fillFunc = function(emptyCollection, collection, n) {
+    var result = [];
 
-    return console.log(collection.fill(emptyCollection, collection, n));
+    for (let i = 0; i < n; i++) {
 
+        result[i] = collection[i];
+        emptyCollection[i] = result[i];
+
+        if (result[i] == undefined) {
+
+            emptyCollection[i] = collection[Math.floor(Math.random() * collection.length)];
+        }
+
+    }
+
+    return emptyCollection;
 }
 
-var printFillFunc = fillFunc([1], [1, 2, 3, 4, "a"], 9)
+var printFillFunc = fillFunc([], [1, 2, 3, 4, "a"], 9)
+console.log(printFillFunc);
+
 
 console.log("--- Reverse Function ---")
 
 var reverseFunc = function(collection) {
 
-    return console.log(collection.reverse());
+    var result = [];
+    for (let i = 0; i < collection.length; i++) {
 
+        var getFirstElement = collection[i];
+        var getLastElement = collection[collection.length - 1 - i];
+
+        getFirstElement = getLastElement;
+        getLastElement = getFirstElement;
+
+        result[i] = getFirstElement;
+        result[i] = getLastElement;
+
+    }
+
+    return result;
 }
 
 var printReverseFunc = reverseFunc([1, 2, 3, 4, 5]);
+console.log(printReverseFunc);
