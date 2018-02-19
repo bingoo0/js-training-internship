6174340eb8fc699e9d6f0b84b03400b4a9ce053b
bingoo0 <mehmedsezgin01@gmail.com>
2018-02-19 14:20:57 +0200

    update template engine

--- templateEngine.js
+++ templateEngine.js
@@ -5,28 +5,22 @@ function templateEngine(placeholder, kvp) {
 
     if (result) {
 
-
         for (let i = 0; i < placeholder.length; i++) {
 
             var match = [];
-
 
             for (var key in kvp) {
 
                 match = pattern.exec(placeholder);
 
                 if (match == null) {
-
                     break;
-
                 }
 
                 var getValue = kvp[key];
 
                 if (key == match[1].trim()) {
-
                     placeholder = placeholder.replace(pattern, getValue);
-
                 }
             }
         }
@@ -40,7 +34,9 @@ var templateObj = {
 
     template: "intern training",
     string: "task",
-    somethingelse: "replace"
+    somethingelse: function() {
+        return "some stcvcvcvcvcing"
+    }
 
 }
 
