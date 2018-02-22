e9b4365658a72bb12c64b415dc9ca9ea20b3790c
bingoo0 <mehmedsezgin01@gmail.com>
2018-02-22 13:39:33 +0200

    add improved todo-list

--- todo-list/index.js
+++ todo-list/index.js
@@ -1,71 +1,97 @@
-var takeLi = "";
+var list = [];
+var createNewTaskElement = function() {
 
-var addToDo = function addInToDo() {
+    var taskInput = document.getElementById("new-task");
+    var addButton = document.getElementsByTagName("button")[0];
+    var incompleteTaskHolder = document.getElementById("incomplete-tasks");
+    var completedTasksHolder = document.getElementById("completed-tasks");
 
-    var li = document.createElement('li');
 
-    var value = document.getElementById("todo").value;
-    var text = document.createTextNode(value);
-    li.appendChild(text);
+    var listItem = document.createElement("li");
+
+    var checkBox = document.createElement("input"); //checkbx
+    //label
+    var label = document.createElement("label"); //label
+    //input (text)
+    var editInput = document.createElement("input"); //text
+    //button.edit
+    var editButton = document.createElement("button"); //edit button
+
+    //button.delete
+    var deleteButton = document.createElement("button"); //delete button
+
+    var value = document.getElementById("new-task").value;
+
+    label.innerText = value;
+
+    listItem.className = "to-do";
+    checkBox.type = "checkbox";
+    checkBox.id = "checkbox";
+    editInput.type = "text";
+
+    editButton.innerText = "Edit";
+    editButton.className = "edit";
+    deleteButton.innerText = "Delete";
+    deleteButton.className = "delete";
 
     if (value === '') {
         alert("You must write something!");
     } else {
-        document.getElementById("ul").appendChild(li);
+
+        listItem.appendChild(checkBox);
+        listItem.appendChild(label);
+        listItem.appendChild(editInput);
+        listItem.appendChild(editButton);
+        listItem.appendChild(deleteButton);
+
+        document.getElementById("tasks-incomplete").appendChild(listItem);
     }
-    document.getElementById("todo").value = "";
 
-    takeLi = li;
 
-    deleteToDo();
-    updateToDo();
-}
+    document.getElementById("new-task").value = "";
 
-var closeToDo = function close() {
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    takeLi.appendChild(span);
+    deleteTask();
+    checkField();
 
 }
 
-var deleteToDo = function delToDo() {
 
-    closeToDo();
-
-    var deleteField = document.getElementsByClassName("close");
+var deleteTask = function() {
+    var deleteField = document.getElementsByClassName("delete");
 
     for (let i = 0; i < deleteField.length; i++) {
         deleteField[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
+            var delField = this.parentElement;
+            delField.style.display = "none";
         }
 
     }
 }
 
+var checkField = function() {
 
-var selectToDoTask = function selectTask() {
+    var checkBox = document.getElementById("checkbox");
+    var completedTasks = document.getElementById("completed-tasks");
+    var incompleteTasks = document.getElementById("tasks-incomplete");
 
-    var list = document.querySelector('UL');
+    checkBox.onchange = function() {
 
-    list.addEventListener('click', function(ev) {
-        if (ev.target.tagName === 'LI') {
-            ev.target.classList.toggle('checked');
+
+        var completed = this.parentElement;
+
+        if (checkBox.checked == true) {
+            checkBox.classList.toggle('checked');
+            completedTasks.appendChild(completed);
+        } else {
+            checkBox.classList.toggle('uncheked');
+            incompleteTasks.appendChild(completed);
         }
-    }, false);
-}
 
-var updateToDo = function() {
+    }
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("EDIT");
-    span.className = "edit";
-    span.appendChild(txt);
-    takeLi.appendChild(span);
-
-    var editField = document.getElementsByClassName("edit");
 
 }
+
+
+
+checkField();
